eda2cb80acfb11b12ea37df18699bfbaca583903
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-11-02 23:59:29 +0100

    chore: create links

---
+++ src/utils/link/create-input-link.ts
@@ -0,0 +1,12 @@
+import { createLink } from './create-link';
+import { EInputs } from '../../core/inputs/inputs.enum';
+
+/**
+ * @description
+ * Utility to create a link based on an input coming from the README.md
+ * @param {Readonly<EInputs>} input The input to link to
+ * @returns {string} A link in the markdown format pointing to the given input anchor in the README.md
+ */
+export function createInputLink(input: Readonly<EInputs>): string {
+  return createLink(input, `https://github.com/@sonia-corporation/stale#${input}`);
+}
