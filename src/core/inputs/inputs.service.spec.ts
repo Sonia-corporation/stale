bd0f06cefe4c6ccefec8f16dce860bb2b72f1e4c
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-11-06 00:08:11 +0100

    test(logger): add more coverage

--- src/core/inputs/inputs.service.spec.ts
+++ src/core/inputs/inputs.service.spec.ts
@@ -1,6 +1,5 @@
 import { IInputs } from './inputs.interface';
 import { InputsService } from './inputs.service';
-import { LoggerFormatService } from '../../utils/logger/logger-format.service';
 import { LoggerService } from '../../utils/logger/logger.service';
 import * as core from '@actions/core';
 
@@ -81,16 +80,12 @@ describe(`InputsService`, (): void => {
     let loggerServiceInfoSpy: jest.SpyInstance;
     let loggerServiceInputSpy: jest.SpyInstance;
     let loggerServiceEndGroupSpy: jest.SpyInstance;
-    let loggerFormatServiceWhiteSpy: jest.SpyInstance;
-    let loggerFormatServiceCyanSpy: jest.SpyInstance;
 
     beforeEach((): void => {
       loggerServiceStartGroupSpy = jest.spyOn(LoggerService, `startGroup`).mockImplementation();
       loggerServiceInfoSpy = jest.spyOn(LoggerService, `info`);
       loggerServiceInputSpy = jest.spyOn(LoggerService, `input`);
       loggerServiceEndGroupSpy = jest.spyOn(LoggerService, `endGroup`).mockImplementation();
-      loggerFormatServiceWhiteSpy = jest.spyOn(LoggerFormatService, `white`);
-      loggerFormatServiceCyanSpy = jest.spyOn(LoggerFormatService, `cyan`);
     });
 
     it(`should create a group of logs`, (): void => {
@@ -108,14 +103,12 @@ describe(`InputsService`, (): void => {
       });
 
       it(`should not log`, (): void => {
-        expect.assertions(4);
+        expect.assertions(2);
 
         InputsService.logInputs();
 
         expect(loggerServiceInfoSpy).not.toHaveBeenCalled();
         expect(loggerServiceInputSpy).not.toHaveBeenCalled();
-        expect(loggerFormatServiceWhiteSpy).not.toHaveBeenCalled();
-        expect(loggerFormatServiceCyanSpy).not.toHaveBeenCalled();
       });
     });
 
@@ -127,7 +120,7 @@ describe(`InputsService`, (): void => {
       });
 
       it(`should log the github token input`, (): void => {
-        expect.assertions(8);
+        expect.assertions(4);
 
         InputsService.logInputs();
 
@@ -135,10 +128,6 @@ describe(`InputsService`, (): void => {
         expect(loggerServiceInfoSpy).toHaveBeenCalledWith(`white-├──`, `input-github-token`, `cyan-dummy-github-token`);
         expect(loggerServiceInputSpy).toHaveBeenCalledTimes(1);
         expect(loggerServiceInputSpy).toHaveBeenCalledWith(`github-token`);
-        expect(loggerFormatServiceWhiteSpy).toHaveBeenCalledTimes(1);
-        expect(loggerFormatServiceWhiteSpy).toHaveBeenCalledWith(`├──`);
-        expect(loggerFormatServiceCyanSpy).toHaveBeenCalledTimes(1);
-        expect(loggerFormatServiceCyanSpy).toHaveBeenCalledWith(`dummy-github-token`);
       });
     });
 
