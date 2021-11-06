96d5b4182c99296e1c133fe4966576f2de1fe134
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-11-06 13:13:26 +0100

    feat(error): catch and stop the action in case of errors

--- src/core/stale.service.ts
+++ src/core/stale.service.ts
@@ -1,8 +1,27 @@
 import { InputsService } from './inputs/inputs.service';
+import { LoggerService } from '../utils/logger/logger.service';
+import * as core from '@actions/core';
 
 export class StaleService {
   public static initialize(): StaleService {
-    InputsService.initialize();
+    try {
+      InputsService.initialize();
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        LoggerService.error(`[${error.name}] ${error.message}`);
+
+        if (error.stack) {
+          LoggerService.debug(error.stack);
+        }
+
+        core.setFailed(`Stale action failed with error ${error.message}`);
+      } else {
+        const errorMessage = `Stale action failed with error ${error}`;
+
+        LoggerService.error(errorMessage);
+        core.setFailed(errorMessage);
+      }
+    }
 
     return StaleService;
   }
