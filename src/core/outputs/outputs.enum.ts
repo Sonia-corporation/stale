6c8390fe3b5fceb9e4094d10a87b3c7b2bcdfaec
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-11-16 00:12:11 +0100

    feat(outputs): add some outputs to this action

---
+++ src/core/outputs/outputs.enum.ts
@@ -0,0 +1,8 @@
+export enum EOutputs {
+  ALREADY_STALE_ISSUES_COUNT = `already-stale-issues-count`,
+  IGNORED_ISSUES_COUNT = `ignored-issues-count`,
+  UNALTERED_ISSUES_COUNT = `unaltered-issues-count`,
+  STALE_ISSUES_COUNT = `stale-issues-count`,
+  PROCESSED_ISSUES_COUNT = `processed-issues-count`,
+  REMOVE_STALE_ISSUES_COUNT = `remove-stale-issues-count`,
+}
--- src/core/stale.service.ts
+++ src/core/stale.service.ts
@@ -1,5 +1,6 @@
 import { InputsService } from '@core/inputs/inputs.service';
 import { IssuesService } from '@core/issues/issues.service';
+import { OutputsService } from '@core/outputs/outputs.service';
 import { StatisticsService } from '@core/statistics/statistics.service';
 import { OctokitService } from '@github/octokit/octokit.service';
 import { LoggerFormatService } from '@utils/loggers/logger-format.service';
@@ -16,6 +17,7 @@ export class StaleService {
       await IssuesService.process();
       LoggerService.info(LoggerFormatService.green(`The stale processing is over`));
       StatisticsService.logsAllStatistics();
+      OutputsService.setOutputs();
     } catch (error: unknown) {
       if (error instanceof Error) {
         LoggerService.error(`[${error.name}] ${error.message}`);
