288ae3b58aae1e7999daab092e8a2b0a39843bc5
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-12-12 19:26:05 +0100

    refactor(inputs): drastically change the inputs service
    now it is just a wrapper of sub-services to handle and log the inputs

---
+++ src/core/inputs/inputs.service.spec.ts
@@ -0,0 +1,107 @@
+import { CommonInputsService } from '@core/inputs/common-inputs.service';
+import { InputsService } from '@core/inputs/inputs.service';
+import { IssuesInputsService } from '@core/inputs/issues-inputs.service';
+import { PullRequestsInputsService } from '@core/inputs/pull-requests-inputs.service';
+
+jest.mock(`@utils/loggers/logger.service`);
+jest.mock(`@utils/loggers/logger-format.service`);
+
+describe(`InputsService`, (): void => {
+  describe(`initialize()`, (): void => {
+    let commonInputsServiceInitializeSpy: jest.SpyInstance;
+    let issuesInputsServiceServiceInitializeSpy: jest.SpyInstance;
+    let pullRequestsInputsServiceServiceInitializeSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      commonInputsServiceInitializeSpy = jest.spyOn(CommonInputsService, `initialize`).mockImplementation();
+      issuesInputsServiceServiceInitializeSpy = jest.spyOn(IssuesInputsService, `initialize`).mockImplementation();
+      pullRequestsInputsServiceServiceInitializeSpy = jest
+        .spyOn(PullRequestsInputsService, `initialize`)
+        .mockImplementation();
+    });
+
+    it(`should initialize the common inputs service`, (): void => {
+      expect.assertions(2);
+
+      InputsService.initialize();
+
+      expect(commonInputsServiceInitializeSpy).toHaveBeenCalledTimes(1);
+      expect(commonInputsServiceInitializeSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should initialize the issues inputs service`, (): void => {
+      expect.assertions(2);
+
+      InputsService.initialize();
+
+      expect(issuesInputsServiceServiceInitializeSpy).toHaveBeenCalledTimes(1);
+      expect(issuesInputsServiceServiceInitializeSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should initialize the pull request inputs service`, (): void => {
+      expect.assertions(2);
+
+      InputsService.initialize();
+
+      expect(pullRequestsInputsServiceServiceInitializeSpy).toHaveBeenCalledTimes(1);
+      expect(pullRequestsInputsServiceServiceInitializeSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should return the service`, (): void => {
+      expect.assertions(1);
+
+      const result = InputsService.initialize();
+
+      expect(result).toStrictEqual(InputsService);
+    });
+  });
+
+  describe(`logInputs()`, (): void => {
+    let commonInputsServiceLogInputsSpy: jest.SpyInstance;
+    let issuesInputsServiceServiceLogInputsSpy: jest.SpyInstance;
+    let pullRequestsInputsServiceServiceLogInputsSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      commonInputsServiceLogInputsSpy = jest.spyOn(CommonInputsService, `logInputs`).mockImplementation();
+      issuesInputsServiceServiceLogInputsSpy = jest.spyOn(IssuesInputsService, `logInputs`).mockImplementation();
+      pullRequestsInputsServiceServiceLogInputsSpy = jest
+        .spyOn(PullRequestsInputsService, `logInputs`)
+        .mockImplementation();
+    });
+
+    it(`should log the common inputs`, (): void => {
+      expect.assertions(2);
+
+      InputsService.logInputs();
+
+      expect(commonInputsServiceLogInputsSpy).toHaveBeenCalledTimes(1);
+      expect(commonInputsServiceLogInputsSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should log the issues inputs`, (): void => {
+      expect.assertions(2);
+
+      InputsService.logInputs();
+
+      expect(issuesInputsServiceServiceLogInputsSpy).toHaveBeenCalledTimes(1);
+      expect(issuesInputsServiceServiceLogInputsSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should log the pull request inputs`, (): void => {
+      expect.assertions(2);
+
+      InputsService.logInputs();
+
+      expect(pullRequestsInputsServiceServiceLogInputsSpy).toHaveBeenCalledTimes(1);
+      expect(pullRequestsInputsServiceServiceLogInputsSpy).toHaveBeenCalledWith();
+    });
+
+    it(`should return the service`, (): void => {
+      expect.assertions(1);
+
+      const result = InputsService.logInputs();
+
+      expect(result).toStrictEqual(InputsService);
+    });
+  });
+});
--- src/core/inputs/inputs.service.ts
+++ src/core/inputs/inputs.service.ts
@@ -1,95 +1,21 @@
-import { EInputs } from '@core/inputs/inputs.enum';
-import { IInputs } from '@core/inputs/inputs.interface';
-import { LoggerFormatService } from '@utils/loggers/logger-format.service';
-import { LoggerService } from '@utils/loggers/logger.service';
-import { isFiniteNumber } from '@utils/numbers/is-finite-number';
-import { ETreeRows } from '@utils/trees/tree-rows.enum';
-import * as core from '@actions/core';
-import { InputOptions } from '@actions/core';
-import _ from 'lodash';
+import { CommonInputsService } from '@core/inputs/common-inputs.service';
+import { IssuesInputsService } from '@core/inputs/issues-inputs.service';
+import { PullRequestsInputsService } from '@core/inputs/pull-requests-inputs.service';
 
-/**
- * @description
- * Used to get the inputs coming from action
- */
 export class InputsService {
-  public static inputs$$: IInputs | undefined = undefined;
-
   public static initialize(): InputsService {
-    InputsService.setInputs();
-    InputsService.logInputs();
+    CommonInputsService.initialize();
+    IssuesInputsService.initialize();
+    PullRequestsInputsService.initialize();
 
     return InputsService;
-  }
-
-  public static setInputs(): IInputs {
-    InputsService.inputs$$ = {
-      dryRun: core.getBooleanInput(EInputs.DRY_RUN, { required: false }),
-      githubToken: core.getInput(EInputs.GITHUB_TOKEN, { required: false }),
-      issueCloseComment: core.getInput(EInputs.ISSUE_CLOSE_COMMENT, { required: false }),
-      issueDaysBeforeClose: this.getNumberInput$$(EInputs.ISSUE_DAYS_BEFORE_CLOSE, { required: false }),
-      issueDaysBeforeStale: this.getNumberInput$$(EInputs.ISSUE_DAYS_BEFORE_STALE, { required: false }),
-      issueIgnoreAllAssignees: core.getBooleanInput(EInputs.ISSUE_IGNORE_ALL_ASSIGNEES, { required: false }),
-      issueIgnoreAllLabels: core.getBooleanInput(EInputs.ISSUE_IGNORE_ALL_LABELS, { required: false }),
-      issueIgnoreAllProjectCards: core.getBooleanInput(EInputs.ISSUE_IGNORE_ALL_PROJECT_CARDS, { required: false }),
-      issueIgnoreAnyAssignees: core.getMultilineInput(EInputs.ISSUE_IGNORE_ANY_ASSIGNEES, { required: false }),
-      issueIgnoreAnyLabels: core.getMultilineInput(EInputs.ISSUE_IGNORE_ANY_LABELS, { required: false }),
-      issueIgnoreBeforeCreationDate: core.getInput(EInputs.ISSUE_IGNORE_BEFORE_CREATION_DATE, { required: false }),
-      issueStaleComment: core.getInput(EInputs.ISSUE_STALE_COMMENT, { required: false }),
-      issueStaleLabel: core.getInput(EInputs.ISSUE_STALE_LABEL, { required: false }),
-    };
-
-    return InputsService.inputs$$;
   }
 
   public static logInputs(): InputsService {
-    LoggerService.startGroup(`Inputs`);
-
-    _.forIn(
-      InputsService.inputs$$,
-      (
-        value: Readonly<string | number | boolean | string[]>,
-        inputName: Readonly<string>,
-        inputs: Readonly<IInputs>
-      ): void => {
-        const lastInputName: string | undefined = _.findLastKey(inputs, (): true => true);
-
-        LoggerService.info(
-          LoggerFormatService.white(inputName === lastInputName ? ETreeRows.LAST : ETreeRows.ANY),
-          LoggerService.input(_.kebabCase(inputName) as EInputs),
-          LoggerService.value(value)
-        );
-      }
-    );
-
-    LoggerService.endGroup();
+    CommonInputsService.logInputs();
+    IssuesInputsService.logInputs();
+    PullRequestsInputsService.logInputs();
 
     return InputsService;
-  }
-
-  public static getInputs(): IInputs | never {
-    if (!InputsService.inputs$$) {
-      throw new Error(`The inputs are unset`);
-    }
-
-    return InputsService.inputs$$;
-  }
-
-  public static getNumberInput$$(input: Readonly<EInputs>, options?: Readonly<InputOptions>): number {
-    const inputValue: string = core.getInput(input, options);
-    const value: number = _.parseInt(inputValue);
-
-    if (!isFiniteNumber(value)) {
-      LoggerService.error(
-        `Wrong value given to the input`,
-        LoggerService.value(input),
-        LoggerFormatService.white(`->`),
-        LoggerService.value(inputValue)
-      );
-
-      throw new Error(`Wrong value given to the input number ${input}`);
-    }
-
-    return value;
   }
 }
