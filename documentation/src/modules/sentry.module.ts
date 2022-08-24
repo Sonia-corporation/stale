b221ce4945f3971057220b6845f52da25a99efe4
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2022-08-24 22:56:57 +0200

    docs(website): configure Sentry
    Closes #843.

---
+++ documentation/src/modules/sentry.module.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-unresolved
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from '@sentry/tracing';
+
+console.debug(`Sentry module initialized`);
+
+if (ExecutionEnvironment.canUseDOM) {
+  console.debug(`Initializing Sentry...`);
+
+  Sentry.init({
+    dsn: `https://3e75b18963924b688011fda3e2b75f4e@o1376500.ingest.sentry.io/6685609`,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+
+  console.debug(`Sentry initialized`);
+}
