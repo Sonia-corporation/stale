dc160b319d2e848ba1c1cdcada5a156d730265c9
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2022-08-26 00:35:49 +0200

    chore(docusaurus): use the right url

--- documentation/docusaurus.vercel.config.js
+++ documentation/docusaurus.vercel.config.js
@@ -1,5 +1,6 @@
 const config = require(`./docusaurus.core.config.js`);
 
 config.clientModules = [require.resolve(`./src/modules/sentry.module.ts`)];
+config.url = `https://sonia-stale-action.vercel.app`;
 
 module.exports = config;
