929efa7997e4a339a8778d094f3923361b66f0b6
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2022-08-26 23:12:29 +0200

    feat(stats): add new stats about the removed issues labels
    Closes #858.

--- src/core/statistics/issues-statistics.service.spec.ts
+++ src/core/statistics/issues-statistics.service.spec.ts
@@ -47,7 +47,7 @@ describe(`IssuesStatisticsService`, (): void => {
 
   describe(`initialize()`, (): void => {
     it(`should reset all the statistics to 0`, (): void => {
-      expect.assertions(11);
+      expect.assertions(12);
       service.processedIssuesCount = 1;
       service.ignoredIssuesCount = 1;
       service.unalteredIssuesCount = 1;
@@ -57,6 +57,7 @@ describe(`IssuesStatisticsService`, (): void => {
       service.closedIssuesCount = 1;
       service.addedIssuesCommentsCount = 1;
       service.addedIssuesLabelsCount = 1;
+      service.removedIssuesLabelsCount = 1;
       service.calledApiIssuesQueriesCount = 1;
       service.calledApiIssuesMutationsCount = 1;
 
@@ -71,6 +72,7 @@ describe(`IssuesStatisticsService`, (): void => {
       expect(service.closedIssuesCount).toBe(0);
       expect(service.addedIssuesCommentsCount).toBe(0);
       expect(service.addedIssuesLabelsCount).toBe(0);
+      expect(service.removedIssuesLabelsCount).toBe(0);
       expect(service.calledApiIssuesQueriesCount).toBe(0);
       expect(service.calledApiIssuesMutationsCount).toBe(0);
     });
@@ -237,7 +239,7 @@ describe(`IssuesStatisticsService`, (): void => {
   });
 
   describe(`increaseAddedIssuesLabelsCount()`, (): void => {
-    it(`should increase the added issues comments count`, (): void => {
+    it(`should increase the added issues labels count`, (): void => {
       expect.assertions(1);
       service.addedIssuesLabelsCount = 0;
 
@@ -247,7 +249,7 @@ describe(`IssuesStatisticsService`, (): void => {
     });
 
     describe.each([0, 1, 2])(`when a specific count is given`, (count: number): void => {
-      it(`should increase the added issues comments count by the given count`, (): void => {
+      it(`should increase the added issues labels count by the given count`, (): void => {
         expect.assertions(1);
         service.addedIssuesLabelsCount = 0;
 
@@ -261,6 +263,36 @@ describe(`IssuesStatisticsService`, (): void => {
       expect.assertions(1);
 
       const result = service.increaseAddedIssuesLabelsCount();
+
+      expect(result).toStrictEqual(service);
+    });
+  });
+
+  describe(`increaseRemovedIssuesLabelsCount()`, (): void => {
+    it(`should increase the removed issues labels count`, (): void => {
+      expect.assertions(1);
+      service.removedIssuesLabelsCount = 0;
+
+      service.increaseRemovedIssuesLabelsCount();
+
+      expect(service.removedIssuesLabelsCount).toBe(1);
+    });
+
+    describe.each([0, 1, 2])(`when a specific count is given`, (count: number): void => {
+      it(`should increase the removed issues labels count by the given count`, (): void => {
+        expect.assertions(1);
+        service.removedIssuesLabelsCount = 0;
+
+        service.increaseRemovedIssuesLabelsCount(count);
+
+        expect(service.removedIssuesLabelsCount).toBe(count);
+      });
+    });
+
+    it(`should return the service`, (): void => {
+      expect.assertions(1);
+
+      const result = service.increaseRemovedIssuesLabelsCount();
 
       expect(result).toStrictEqual(service);
     });
@@ -335,6 +367,7 @@ describe(`IssuesStatisticsService`, (): void => {
         service.closedIssuesCount = 0;
         service.addedIssuesCommentsCount = 0;
         service.addedIssuesLabelsCount = 0;
+        service.removedIssuesLabelsCount = 0;
         service.calledApiIssuesQueriesCount = 0;
         service.calledApiIssuesMutationsCount = 0;
       });
@@ -359,6 +392,7 @@ describe(`IssuesStatisticsService`, (): void => {
         service.closedIssuesCount = 0;
         service.addedIssuesCommentsCount = 0;
         service.addedIssuesLabelsCount = 0;
+        service.removedIssuesLabelsCount = 0;
         service.calledApiIssuesQueriesCount = 0;
         service.calledApiIssuesMutationsCount = 0;
       });
@@ -384,16 +418,17 @@ describe(`IssuesStatisticsService`, (): void => {
         service.closedIssuesCount = 6;
         service.addedIssuesCommentsCount = 7;
         service.addedIssuesLabelsCount = 8;
-        service.calledApiIssuesQueriesCount = 9;
-        service.calledApiIssuesMutationsCount = 10;
+        service.removedIssuesLabelsCount = 9;
+        service.calledApiIssuesQueriesCount = 10;
+        service.calledApiIssuesMutationsCount = 11;
       });
 
       it(`should log the statistics`, (): void => {
-        expect.assertions(12);
+        expect.assertions(14);
 
         service.logsAllStatistics();
 
-        expect(loggerServiceInfoSpy).toHaveBeenCalledTimes(11);
+        expect(loggerServiceInfoSpy).toHaveBeenCalledTimes(13);
         expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
           1,
           `white-├──`,
@@ -439,26 +474,70 @@ describe(`IssuesStatisticsService`, (): void => {
         expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
           8,
           `white-├──`,
+          `whiteBright-Issues labels        `,
+          `value-17`
+        );
+        expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
+          9,
+          `white-│   ├──`,
           `whiteBright-Added issues labels  `,
           `value-8`
         );
         expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
-          9,
-          `white-└──`,
-          `whiteBright-Called API issues    `,
-          `value-19`
-        );
-        expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
           10,
-          `white-    ├──`,
-          `whiteBright-Called API issues queries  `,
+          `white-│   └──`,
+          `whiteBright-Removed issues labels`,
           `value-9`
         );
         expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
           11,
+          `white-└──`,
+          `whiteBright-Called API issues    `,
+          `value-21`
+        );
+        expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
+          12,
+          `white-    ├──`,
+          `whiteBright-Called API issues queries  `,
+          `value-10`
+        );
+        expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
+          13,
           `white-    └──`,
           `whiteBright-Called API issues mutations`,
-          `value-10`
+          `value-11`
+        );
+      });
+    });
+
+    describe(`when there is one added issues labels count statistic`, (): void => {
+      beforeEach((): void => {
+        service.processedIssuesCount = 0;
+        service.ignoredIssuesCount = 0;
+        service.unalteredIssuesCount = 0;
+        service.staleIssuesCount = 0;
+        service.removeStaleIssuesCount = 0;
+        service.alreadyStaleIssuesCount = 0;
+        service.closedIssuesCount = 0;
+        service.addedIssuesCommentsCount = 0;
+        service.addedIssuesLabelsCount = 1;
+        service.removedIssuesLabelsCount = 0;
+        service.calledApiIssuesQueriesCount = 0;
+        service.calledApiIssuesMutationsCount = 0;
+      });
+
+      it(`should log the added issues labels count statistic but not the removed issues labels count statistic`, (): void => {
+        expect.assertions(3);
+
+        service.logsAllStatistics();
+
+        expect(loggerServiceInfoSpy).toHaveBeenCalledTimes(2);
+        expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(1, `white-└──`, `whiteBright-Issues labels`, `value-1`);
+        expect(loggerServiceInfoSpy).toHaveBeenNthCalledWith(
+          2,
+          `white-    └──`,
+          `whiteBright-Added issues labels`,
+          `value-1`
         );
       });
     });
@@ -474,6 +553,7 @@ describe(`IssuesStatisticsService`, (): void => {
         service.closedIssuesCount = 0;
         service.addedIssuesCommentsCount = 0;
         service.addedIssuesLabelsCount = 0;
+        service.removedIssuesLabelsCount = 0;
         service.calledApiIssuesQueriesCount = 1;
         service.calledApiIssuesMutationsCount = 0;
       });
