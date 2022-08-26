e32ce01db7e6022eb0324a44b835f5a51e10ca24
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2022-08-26 18:57:35 +0200

    chore(docusaurus): fix wraning about wrong url

--- documentation/docusaurus.github.config.js
+++ documentation/docusaurus.github.config.js
@@ -2,6 +2,6 @@ const config = require(`./docusaurus.core.config.js`);
 
 config.baseUrl = `/stale/`;
 config.deploymentBranch = `gh-pages`;
-config.url = `https://sonia-corporation.github.io/stale/`;
+config.url = `https://sonia-corporation.github.io`;
 
 module.exports = config;
