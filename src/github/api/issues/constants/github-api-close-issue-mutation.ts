98fa64b093dda62527546fab0eb7ab1fd219b684
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2022-08-31 19:30:21 +0200

    fix(issues): fix closing issues bug due to the close reason feature
    This issue is present since the version 2.0.0.
    Fixes #886.

--- src/github/api/issues/constants/github-api-close-issue-mutation.ts
+++ src/github/api/issues/constants/github-api-close-issue-mutation.ts
@@ -1,5 +1,5 @@
 export const GITHUB_API_CLOSE_ISSUE_MUTATION = `
-  mutation CloseIssue($issueId: ID!) {
+  mutation CloseIssue($issueId: ID!, $reason: IssueClosedStateReason!) {
     __typename
     closeIssue(input: {issueId: $issueId, stateReason: $reason}) {
       clientMutationId
